1ad1317beb3b675f21d47db8468b41a3af139f6e
Joshua Liew <liqshann@gmail.com>
2025-08-09 16:04:58 +0900

    feat: get_count_chars returns dict with char count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_count_words
+from stats import get_count_words, get_count_chars
 
 
 def get_book_text(filepath):
@@ -9,8 +9,11 @@ def get_book_text(filepath):
 
 def main():
     text = get_book_text("./books/frankenstein.txt")
-    count = get_count_words(text)
-    print(F"{count} words found in the document")
+    num_words = get_count_words(text)
+    print(F"{num_words} words found in the document")
+
+    num_chars = get_count_chars(text)
+    print(num_chars)
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_count_words(text):
     count = len(text.split())
     return count
+
+
+def get_count_chars(text):
+    res = {}
+    for char in text.lower():
+        if char not in res:
+            res[char] = 0
+        res[char] += 1
+    return res
